1cc9632d5c49467a044b8505da0c267633ebbbb0
NicholasKhatch <nicholas.khatchadourian23@ycdsbk12.ca>
2022-01-09 15:25:48 -0500

    completed all quadrants, final version 1

--- Sketch.java
+++ Sketch.java
@@ -16,20 +16,54 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(255,255,255);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    background(100);
+    stroke(0);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    for (int lineX = width/20; lineX <= width/2; lineX += width/20){
+      line(lineX, 0, lineX, width/2);
+    }
+    for (int lineY = height/20; lineY <= height/2; lineY += height/20){
+      line(0, lineY, height/2, lineY);
+    }
+
+    for (int circleY = 0; circleY <= (height/2) - (height/40); circleY += height/10){
+      for (int circleX = width - (width/10); circleX >= width/2; circleX -= height/10){
+        fill(255);
+        ellipse(circleX + 20, circleY + 20, (width/10) - width/40, (height/10) - height/40);
+      }
+    }
+
+    for (int colourval = 0; colourval < (width/2); colourval++){
+      stroke(colourval,colourval,colourval);
+      line(colourval,200,colourval,height);
+    }
+
+    fill(232, 120, 245);
+    for (int petalX = (int) (width/1.42857142857); petalX <= (width/1.25); petalX += (width/40)){
+        if (petalX == (280)){
+          ellipse(petalX, petalX + (float) (width/26.6666666667), width/16, height/16);
+          ellipse(petalX, petalX + width/16, width/16, width/16);
+        } else if (petalX == 290){
+          ellipse(petalX, petalX - width/80, width/16, width/16);
+          ellipse(petalX, petalX + width/16, width/16, width/16);
+        } else if (petalX == 310){
+          ellipse(petalX, petalX - width/16, width/16, width/16);
+          ellipse(petalX, petalX + width/80, width/16, width/16);
+        } else if (petalX == width/1.25){
+          ellipse(petalX, (float) (petalX - (width/26.6666666667)), width/16, width/16);
+          ellipse(petalX, petalX - width/16, width/16, width/16);
+        }
+    }
+    fill(245, 201, 120);
+    ellipse(3*(width/4), 3*(height/4), (width/10) - width/40, (height/10) - height/40);
+
   }
   
   // define other methods down here.
